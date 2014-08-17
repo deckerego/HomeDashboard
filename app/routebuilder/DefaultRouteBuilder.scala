676ce6251d120b68a86b342f8b7b5d6d9a405aee
John Ellis <deckerego@gmail.com>
2014-08-17 19:49:50 -0400

    Working XMPP implementation using Camel

--- app/routebuilder/DefaultRouteBuilder.scala
+++ app/routebuilder/DefaultRouteBuilder.scala
@@ -2,9 +2,14 @@ package routebuilder
 
 import org.apache.camel.{Exchange, Processor}
 import org.apache.camel.builder.RouteBuilder
+import com.typesafe.config.ConfigFactory
 
 class DefaultRouteBuilder extends RouteBuilder {
+  val conf = ConfigFactory.load();
+  val xmppConf = conf.getConfig("xmpp")
+
   def configure {
-    from("direct:talk").to("xmpp://talk.google.com:5222/")
+    from("direct:talk").
+      to(s"xmpp://${xmppConf.getString("server")}:${xmppConf.getInt("port")}/${xmppConf.getString("recipient")}?serviceName=${xmppConf.getString("name")}&user=${xmppConf.getString("username")}&password=${xmppConf.getString("password")}")
   }
 }
